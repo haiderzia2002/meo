0d6283ef8acc7a6728e4e215c6ee13848df18789
haiderzia2002 <haiderzia2002@hotmail.com>
2017-02-28 19:49:52 +0300

    v2.0.0
    Push v2.0.0. Likely the end of active development as focus goes towards
    other projects.

--- meo.js
+++ meo.js
@@ -1,8 +1,16 @@
-/* meo Player *
- *   v1.7.0   *
- *   LGPLv3   */
+/* meo media Player *
+ * v2.0.0 || LGPLv3 *
+ * by haiderzia2002 */
 $(document).ready(function() {
-	$.fn.meo = function() {
+	$.fn.meo = function(options) {
+		// Setting default options
+		var settings = $.extend({
+			timeFormat: "default",
+			hideTime: "2000",
+			skip: "5"
+		}, options);
+
+		// Pauses and plays the video
 		$.fn.handlePlay = function() {
 			var med = this.get(0);
 			if (med.paused || med.ended) {
@@ -12,56 +20,66 @@ $(document).ready(function() {
 			};
 		};
 
+		// Converts seconds to either HH:MM:SS or MM:SS format
 		$.fn.time = function(a) {
-			if (this.duration >= 3600) {
-				var h = Math.floor(a / 3600) < 10 ? "0" + Math.floor(a / 3600) : Math.floor(a / 3600);
-				var m = Math.floor(a / 60) % 60 < 10 ? "0" + Math.floor(a / 60) % 60 : Math.floor(a / 60) % 60;
-				var s = Math.floor(a % 60) < 10 ? "0" + Math.floor(a % 60) : Math.floor(a % 60);
+			if (settings.timeFormat == "hhmmss" || (settings.timeFormat == "default" && a >= 3600)) {
+				var h = Math.floor(a / 3600) < 10 ? "0" + Math.floor(a / 3600) : Math.floor(a / 3600),
+				    m = Math.floor(a / 60) % 60 < 10 ? "0" + Math.floor(a / 60) % 60 : Math.floor(a / 60) % 60,
+				    s = Math.floor(a % 60) < 10 ? "0" + Math.floor(a % 60) : Math.floor(a % 60);
 				return h + ":" + m + ":" + s;
-			} else {
-				var m = Math.floor(a / 60) < 10 ? "0" + Math.floor(a / 60) : Math.floor(a / 60);
-				var s = Math.floor(a % 60) < 10 ? "0" + Math.floor(a % 60) : Math.floor(a % 60);
+			} else if (settings.timeFormat == "mmss" || (settings.timeFormat == "default" && a < 3600)) {
+				var m = Math.floor(a / 60) < 10 ? "0" + Math.floor(a / 60) : Math.floor(a / 60),
+				    s = Math.floor(a % 60) < 10 ? "0" + Math.floor(a % 60) : Math.floor(a % 60);
 				return m + ":" + s;
 			};
 		};
 
+		// Test whether to use fallback
 		var meot1 = document.createElement("video").canPlayType;
-		var meot2 = !navigator.userAgent.match(/(iPod|iPhone|iPad)/i);
 		return this.each(function() {
-			var med = $(this);
+			var med = $(this),
+			    men = this;
 			med.wrap('<div class="meo"></div>');
 			var meocn = med.parent();
-			if (meot1  && meot2) {
+			if (meot1) {
+				// Insert controls and disable browser default controls
 				med.after('<ul tabindex="-1"><li class="playp"></li><li class="ctime"></li><li class="progr"><div class="progl"></div><div class="progb"></div><div class="ftime"></div><div class="ftimt"></div></li><li class="ttime"></li><li class="fs"></li></ul>');
 				this.controls = false;
-				var meoul = med.siblings();
-				var playp = meoul.find(".playp");
-				var ctime = meoul.find(".ctime");
-				var progr = meoul.find(".progr");
-				var progb = meoul.find(".progb");
-				var progl = meoul.find(".progl");
-				var ftime = meoul.find(".ftime");
-				var ftimt = meoul.find(".ftimt");
-				var ttime = meoul.find(".ttime");
-				var fs = meoul.find(".fs");
+				// Enable video on iOS 10 iPhone to play without going into fullscreen
+				med.attr("playsinline", "true");
+				
+				// Declare variables
+				var meoul = med.siblings(),
+				    playp = meoul.find(".playp"), // play/pause button
+				    ctime = meoul.find(".ctime"), // current time
+				    progr = meoul.find(".progr"), // progress played
+				    progb = meoul.find(".progb"), // whole progress bar
+				    progl = meoul.find(".progl"), // progress loaded
+				    ftime = meoul.find(".ftime"), // time float
+				    ftimt = meoul.find(".ftimt"), // time float pinter
+				    ttime = meoul.find(".ttime"), // total time
+				    fs = meoul.find(".fs"); // fullscreen button
 
+				// Size the controls/Init time values
 				med.on("loadedmetadata", function() {
 					ctime.text(med.time(this.currentTime));
 					ttime.text(med.time(this.duration));
 					med.on("loadeddata", function() {
-						var conwidth = playp.outerWidth(true) + ctime.outerWidth(true) + (progr.outerWidth(true) - progr.outerWidth(false)) + ttime.outerWidth(true) + fs.outerWidth(true);
+						var conwidth = playp.outerWidth(true) + ctime.outerWidth(true) + progr.outerWidth(true) - progr.outerWidth(false) + ttime.outerWidth(true) + fs.outerWidth(true);
 						progr.css("width", med.width() - conwidth);
-						med.on("progress", function() {
-							progl.width(((this.buffered.end(0) / this.duration) * 100) + "%");
-						});
+						progl.width(((this.buffered.end(0) / this.duration) * 100) + "%");
 					});
 				});
 
+				// Update time values and resize controls
 				med.on("timeupdate", function() {
+					var conwidth = playp.outerWidth(true) + ctime.outerWidth(true) + progr.outerWidth(true) - progr.outerWidth(false) + ttime.outerWidth(true) + fs.outerWidth(true);
 					progb.width(((this.currentTime / this.duration) * 100) + "%");
 					ctime.text(med.time(this.currentTime));
+					progr.css("width", med.width() - conwidth);
 				});
 
+				// Handles changing icon on play/pause button
 				med.on("pause", function() {
 					playp.css("background-position", "0% 0%");
 				});
@@ -70,22 +88,25 @@ $(document).ready(function() {
 					playp.css("background-position", "0% 100%");
 				});
 
+				// Makes the play/payse button function
 				playp.css("background-position", "0% 0%");
 				playp.click(function() {
 					med.handlePlay();
 				});
 
+				// Jump to point on progress bar
 				progr.click(function(e) {
 					var pos = (e.pageX - progr.offset().left) / progr.width();
-					med.get(0).currentTime = pos * med.get(0).duration;
+					men.currentTime = pos * men.duration;
 				});
 
+				// Time float
 				progr.mousemove(function(e) {
 					meoul.find(".ftime, .ftimt").css("visibility", "visible");
 					ftime.css("visibility", "visible");
 					var pos = (e.pageX - progr.offset().left) / progr.width();
 					if (pos >= 0 && pos <= 1) {
-						ftime.text(med.time(pos * med.get(0).duration));
+						ftime.text(med.time(pos * men.duration));
 						ftime.css("left", pos * progr.width() - (ftime.outerWidth(true) / 2));
 						ftimt.css("left", pos * progr.width() - (ftimt.outerWidth(true) / 2));
 					};
@@ -95,14 +116,20 @@ $(document).ready(function() {
 					meoul.find(".ftime, .ftimt").css("visibility", "hidden");
 				});
 
+				// Keyboard controls
 				meocn.keydown(function(e) {
-					if (e.which == 32) {
+					if (e.which == 32 || e.which == 75) { // spacebar || k
 						e.preventDefault();
 						meocn.find("video, audio").handlePlay();
+					} else if (e.which == 37 || e.which == 74) { // ArrowLeft || j
+						men.currentTime -= settings.skip;
+					} else if (e.which == 32 || e.which == 76) { // ArrowRight || l
+						men.currentTime -= -1 * settings.skip;
 					};
 				});
 
 				if (med.is("video")) {
+					// Fullscreen
 					fs.css("background-position", "100% 0%");
 					fs.on("click", function() {
 						if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
@@ -111,13 +138,12 @@ $(document).ready(function() {
 							} else if (document.webkitExitFullscreen) {
 								document.webkitExitFullscreen();
 							} else if (document.mozCancelFullScreen) {
-								document.mozCancelFullScreen();					
+								document.mozCancelFullScreen();
 							} else if (document.msExitFullscreen) {
 								document.msExitFullscreen();
 							};
 						} else {
-							meofs = fs.parents(".meo");
-							var meo = meofs.get(0);
+							var meo = meocn.get(0);
 							if (meo.requestFullscreen) {
 								meo.requestFullscreen();
 							} else if (meo.webkitRequestFullscreen) {
@@ -126,57 +152,56 @@ $(document).ready(function() {
 								meo.mozRequestFullScreen();
 							} else if (meo.msRequestFullscreen) {
 								meo.msRequestFullscreen();
+							} else if (men.webkitEnterFullScreen) {
+								men.webkitEnterFullScreen();
 							};
 						};
 					});
 
 					$(document).on("fullscreenchange webkitfullscreenchange mozfullscreenchange msfullscreenchange", function() {
-						var meoul = meofs.find("ul");
-						var med = meofs.find("video");
-						var playp = meofs.find(".playp");
-						var ctime = meofs.find(".ctime");
-						var progr = meofs.find(".progr");
-						var ttime = meofs.find(".ttime");
-						var fs = meofs.find(".fs");
-						meoul.css("visibility", "visible");
 						var conwidth = playp.outerWidth(true) + ctime.outerWidth(true) + (progr.outerWidth(true) - progr.outerWidth(false)) + ttime.outerWidth(true) + fs.outerWidth(true);
 						if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
-							meofs.addClass("meofs");
+							meocn.addClass("meofs");
 							fs.css("background-position", "100% 100%");
 							progr.css("width", screen.width - conwidth);
 						} else {
-							meofs.removeClass("meofs");
+							meocn.removeClass("meofs");
 							fs.css("background-position", "100% 0%");
 							progr.css("width", med.width() - conwidth);
 						};
 					});
 
-					med.click(function() {
-						if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
-							if (meoul.css("visibility") == "visible") {
-								meoul.css("visibility", "hidden");
-							} else {
-								meoul.css("visibility", "visible");
-							};
+					// Hides controlbar
+					hide = 0;
+					med.mousemove(function() {
+						if (men.paused != true) {
+							window.clearTimeout(hide);
+							meoul.removeClass("hidec");
+							hide = window.setTimeout(function(){meoul.addClass("hidec")}, settings.hideTime);
 						};
 					});
+
+					med.on("pause play end mouseleave", function() {
+						window.clearTimeout(hide);
+						meoul.removeClass("hidec");
+					});
 				};
-			} else if (meot2 != true) {
-			} else if (meot1 != true) {
-				meo.addClass("nomed");
+			} else {
+				// Generates fallback
+				meocn.addClass("nomed");
 				med.css("display", "none");
 				var lengt = med.find("source").length;
 				if (lengt > 0) {
 					src = med.find("source");
 					while (lengt > 0) {
 						srcn = src.attr("src")
-						lengt += -1;
+						lengt -= 1;
 						src = src.nextAll("source");
-						meo.append('<a href="' + srcn + '">' + srcn + '</a><br>');
+						meocn.append('<a href="' + srcn + '">' + srcn + '</a><br>');
 					};
 				} else {
 					srcn = med.attr("src");
-					meo.append('<a href="' + srcn + '">' + srcn + '</a>');
+					meocn.append('<a href="' + srcn + '">' + srcn + '</a>');
 				};
 			};
 		});
